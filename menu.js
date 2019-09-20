962f6a712fc880b9619378f9d86136684c2f2bca
Santiago <santiago.chamon@gmail.com>
2019-09-21 07:13:09 +1000

    mobile responsiveness

--- menu.js
+++
@@ -1,9 +0,0 @@
-// Menu popup functionality
-
-let mainNav = document.getElementById('js-menu');
-let navBarToggle = document.getElementById('js-navbar-toggle');
-
-navBarToggle.addEventListener('click', function () {
-
-    mainNav.classList.toggle('active');
-});
